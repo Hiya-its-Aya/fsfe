3f67d5695df9f802a87b5fbef1fea8e8397446b4
Aya Moosa <ayamoosa1@gmail.com>
2024-03-23 13:18:26 -0700

    fix

--- index-ws.js
+++ index-ws.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
   res.sendFile('index.html', { root: __dirname });
 });
 
+
+server.on('request', app);
+server.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
+
+// Begin websocket implementation
+
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({ server: server });
+
 process.on('SIGINT', () => {
   console.log('Shutting down server');
   wss.clients.forEach((client) => {
@@ -16,46 +29,35 @@ process.on('SIGINT', () => {
   }) 
 });
 
-server.on('request', app);
-server.listen(3000, () => {
-  console.log('Server started on port 3000');
+wss.on('connection', (ws) => {
+  const numClients = wss.clients.size;
+  console.log(`Client connected. Total clients: ${numClients}`);
+
+  wss.broadcast('current visitors: ' + numClients);
+
+  if(ws.readyState === ws.OPEN){
+    ws.send('Welcome to my server');
+  }
+
+  db.run(`
+    INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+    `);
+
 });
 
+wss.on('close', () => { 
+  wss.broadcast(`Current visitors: ${numClients}`);
+  console.log(`Client disconnected. Total clients: ${numClients}`);
+});
 
-// Begin websocket implementation
-
- const WebSocketServer = require('ws').Server;
-
- const wss = new WebSocketServer({ server: server });
-
-  wss.on('connection', (ws) => {
-    const numClients = wss.clients.size;
-    console.log(`Client connected. Total clients: ${numClients}`);
-
-    wss.broadcast('current visitors: ' + numClients);
-
-    if(ws.readyState === ws.OPEN){
-      ws.send('Welcome to my server');
+wss.broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    if(client.readyState === client.OPEN){
+      client.send(data);
     }
-
-    db.run(`
-      INSERT INTO visitors (count, time)
-      VALUES (${numClients}, datetime('now'))
-      `);
-
   });
-
-  wss.on('close', () => { 
-    console.log(`Client disconnected. Total clients: ${numClients}`);
-  });
-
-  wss.broadcast = (data) => {
-    wss.clients.forEach((client) => {
-      if(client.readyState === client.OPEN){
-        client.send(data);
-      }
-    });
-  }
+}
 
 
 
